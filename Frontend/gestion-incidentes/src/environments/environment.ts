04822c5bbd4e9a4c13f60522d4318490a277bf09
Rodri-Notebook2 <rodrigomoran143@gmail.com>
2026-05-17 10:40:56 -0300

    update: update url of render in front

--- Frontend/gestion-incidentes/src/environments/environment.ts
+++ Frontend/gestion-incidentes/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
+  apiUrl: 'https://civix-gateway.onrender.com'
   /*
-  apiUrl: 'https://civix-gateway.onrender.com',
-  apiUrlLocal: 'http://localhost:8080'*/
-  apiUrl: 'http://localhost:8080',
+  apiUrlLocal: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',*/
 };
